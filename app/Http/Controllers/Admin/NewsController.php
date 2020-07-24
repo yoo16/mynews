692f5dae9b17761728eb1b2f357cde6835ef90b4
yoo16 <yohei.yoshikawa@gmail.com>
2020-07-24 16:24:59 +0000

    first commit

---
+++ app/Http/Controllers/Admin/NewsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class NewsController extends Controller
+{
+  //追加
+  public function add()
+  {
+    return view('admin.news.create');
+  }
+  
+  public function create()
+  {
+    return redirect('admin/profile/create');
+  }
+  
+  public function edit()
+  {
+    return view('admin.profile.edit');
+  }
+  
+  public function update()
+  {
+    return redirect('admin/profile/edit');
+  }
+  
+}
---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.profile')
+
+@section('title', 'My Profile')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My Profile</h2>
+            </div>
+        </div>
+    </div>
+@endsection
